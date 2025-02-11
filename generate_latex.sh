e0a5f72195a12625a1d0c18a83f5c9615dde3b45
Corey O'Malley <omalley242@gmail.com>
2025-02-11 12:48:49 +0000

    formatting in custom script

--- generate_latex.sh
+++ generate_latex.sh
@@ -2,6 +2,10 @@
 # convert each notebook into markdown -> latex
 for i in ./Notebooks/**/**/*.ipynb; do 
     echo $i
-    jupyter nbconvert --to markdown $i 
+    jupyter nbconvert --to markdown $i
     pandoc --listings -f markdown -t latex "${i%.*}.md" -o "${i%.*}.tex"
+    sed -i -e 's/lstlisting/python/g' "${i%.*}.tex" #convert listing blocks to python
+    sed -i -e 's/\\begin{figure}/\\begin{figure}\[h\]/g' "${i%.*}.tex" #convert listing blocks to python
+    sed -i -e 's/\\includegraphics/\\includegraphics\[scale=0.75\]/g' "${i%.*}.tex" #convert listing blocks to python
+    rm -f "${i%.*}.md" 
 done
